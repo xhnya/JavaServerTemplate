8e95dc4e127a6cba601c3e82e28cb357c6107e70
肖海南 <hain.work@outlook.com>
2024-11-29 09:35:01 +0800

    初始化项目

---
+++ src/test/java/com/xhn/JavaServerTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
